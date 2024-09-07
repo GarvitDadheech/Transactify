69ca5947137bbf63c4740181d6faa111b1056d0b
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-07 23:44:03 +0530

    implement user update

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -19,6 +19,13 @@ const loginBody = zod.object({
     password: zod.string()
 })
 
+const updateBody = zod.object({
+	password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional(),
+})
+
+
 router.post("/signup",async (req,res) => {
     const {success} = signUpBody.safeParse(req.body);
 
@@ -96,5 +103,20 @@ router.post("/login",async (req,res) => {
     })
 
 }) 
+
+router.put("/", authMiddleware, async (req, res) => {
+    const { success } = updateBody.safeParse(req.body)
+    if (!success) {
+        res.status(411).json({
+            message: "Error while updating information"
+        })
+    }
+
+	await User.updateOne({ _id: req.userId }, req.body);
+	
+    res.json({
+        message: "Updated successfully"
+    })
+})
 
 module.exports = router;
