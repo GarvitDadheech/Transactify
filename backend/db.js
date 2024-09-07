e5038dc635e78ddc4075682a0ee512dea803047b
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-07 23:14:29 +0530

    implement user signup

--- backend/db.js
+++ backend/db.js
@@ -1,3 +1,37 @@
 const mongoose = require("mongoose");
 
-mongoose.connecct
+mongoose.connect("mongodb://localhost:27017/paytm");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+});
+
+const User = mongoose.model("User",userSchema);
+
+module.exports = {
+    User
+}
---
+++ backend/routes/userRouter.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const zod = require("zod");
+const { User } = require("../db");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const {JWT_SECRET} = require("../config");
+
+const signUpBody = zod.object({
+    username: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string()
+})
+
+router.post("/signup",async (req,res) => {
+    const {success} = signUpBody.safeParse(req.body);
+
+    if(!success) {
+        return res.status(411).json({
+            message: "Incorrect inputs"
+        });
+    }
+
+    const existingUser = await User.findOne({
+        username: req.body.username
+    })
+
+    if (existingUser) {
+        return res.status(411).json({
+            message: "Username already taken"
+        });
+    }
+
+    const user = await User.create({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+    })
+
+    const userId = user._id;
+
+    const token = jwt.sign({
+        userId
+    }, JWT_SECRET);
+
+    res.json({
+        message: "User created successfully",
+        token: token
+    })
+
+})
+
+module.exports = router;
