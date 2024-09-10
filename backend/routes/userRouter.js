be058a4c83aef3afd260f054819b97bafe3efc7c
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-11 00:46:27 +0530

    add route to get filtered users

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -119,4 +119,26 @@ router.put("/", authMiddleware, async (req, res) => {
     })
 })
 
+router.put("/bulk",async (req,res) => {
+    const filter = req.query.filter || "";
+    const users = User.find({
+        $or: [{
+            firstName: {
+                "$regex": filter
+            },
+            lastName: {
+                "$regex": filter
+            }
+        }]
+    })
+    res.json({
+        user: users.map(user => ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+});
+
 module.exports = router;
