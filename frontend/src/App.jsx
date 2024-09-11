a341ca44478856f3b639fed01d8073d12ce84d27
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-11 12:51:57 +0530

    add router and make pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,22 @@
+import { BrowserRouter, Routes,Route } from 'react-router-dom'
 import './App.css'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Dashboard from './pages/Dashboard'
+import Send from './pages/Send'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="signup" element={<Signup/>}/>
+          <Route path='signin' element={<Signin/>}/>
+          <Route path='dashboard' element={<Dashboard/>}/>
+          <Route path='send' element={<Send/>}/>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
