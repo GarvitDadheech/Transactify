bafca093be979688b32359fb6632eff81c9a0142
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-11 22:45:10 +0530

    balance code

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,7 @@
+export default function Users() {
+    return(
+        <div>
+            <h1 className="text-3xl font-extrabold">Users</h1>
+        </div>
+    )
+}
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,12 @@
 import AppBar from "../components/AppBar";
+import Heading from "../components/Heading";
 
 export default function Dashboard() {
     return (
-        <AppBar/>
+        <div>
+            <AppBar/>
+            <div className="h-0.5 w-screen bg-black mt-4"></div>
+            <h1 className="text-3xl font-bold ml-8 mt-16">Your balance is $5000</h1>
+        </div>
     )
 }
